7f0943b063a8f4bc6cd5a713dbd1d60ec84864ac
Caio Siqueira da Silva <csiqueirasilva@gmail.com>
2016-05-29 23:27:24 -0300

    Added gitignore and did some repository cleanup

---
+++ app/src/main/java/br/uva/goldenservices/MainActivity.java
@@ -0,0 +1,73 @@
+package br.uva.goldenservices;
+
+import android.app.ActionBar;
+import android.app.Activity;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.Window;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.FrameLayout;
+import android.widget.FrameLayout.LayoutParams;
+import android.widget.LinearLayout;
+import android.widget.RadioButton;
+import android.widget.ScrollView;
+public class MainActivity extends Activity 
+{/*
+  ActionBar bar;
+  EditText editNome,editTelefone,editMatricula,editEndereco,editEmail,editConfirmeEmail,editSenha,editConfirmSenha;
+  RadioButton rbtn1,rbtn2;
+ 
+  Button botaoCriar;
+  ScrollView sv;
+  LinearLayout ll;
+ */
+
+    EditText senha;
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) 
+ {
+  super.onCreate(savedInstanceState);
+  super.setContentView(R.layout.login);
+
+     senha = (EditText)findViewById(R.id.senha);
+
+
+
+  
+ /* editNome = (EditText) findViewById(R.id.editNome);
+  editTelefone = (EditText) findViewById(R.id.EditTelefone);
+  editEndereco = (EditText) findViewById(R.id.editEndereco);
+  editMatricula = (EditText) findViewById(R.id.editTMatricula);
+  editEmail  = (EditText) findViewById(R.id.editEmail);
+  editConfirmeEmail = (EditText) findViewById(R.id.editConfirmeEmail);
+  editSenha = (EditText) findViewById(R.id.Senha);
+  editConfirmSenha  = (EditText) findViewById(R.id.cofirmeSenha);
+  sv = (ScrollView) findViewById(R.id.scrollView1);
+  FrameLayout.LayoutParams lpsv = new FrameLayout.LayoutParams(LayoutParams.MATCH_PARENT,LayoutParams.MATCH_PARENT);
+  sv.setLayoutParams(lpsv);
+
+  ll =(LinearLayout)findViewById(R.id.linearLayout);
+  ll.setOrientation(LinearLayout.VERTICAL);
+  LinearLayout.LayoutParams lpll = new  LinearLayout.LayoutParams(LayoutParams.MATCH_PARENT,LayoutParams.MATCH_PARENT);  
+  ll.setLayoutParams(lpll);
+  sv.addView(ll);
+  sv.addView(editNome);
+  sv.addView(editSenha);
+  sv.addView(editTelefone);
+  sv.addView(editConfirmeEmail);
+  sv.addView(editEmail);
+  sv.addView(editConfirmSenha);
+  sv.addView(editMatricula);
+  sv.addView(editEndereco);
+  bar = getActionBar();
+  bar.setBackgroundDrawable(new ColorDrawable(Color.parseColor("#0000ff")));
+  */
+  
+  
+ }
+}
