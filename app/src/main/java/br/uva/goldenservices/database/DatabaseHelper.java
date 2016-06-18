cec49998a15dcaa1a5c0f52ddd48fa6aef5eb0db
Caio Siqueira da Silva <csiqueirasilva@gmail.com>
2016-06-18 11:23:51 -0300

    Added id_usuario column to recibo table

--- app/src/main/java/br/uva/goldenservices/database/DatabaseHelper.java
+++ app/src/main/java/br/uva/goldenservices/database/DatabaseHelper.java
@@ -10,6 +10,8 @@ import java.util.ArrayList;
 import java.util.List;
 
 import br.uva.goldenservices.database.recibos.Recibo;
+import br.uva.goldenservices.ui.Helper;
+import golden.services.model.usuarios.Usuario;
 
 /**
  * Created by csiqueira on 18/06/16.
@@ -19,12 +21,12 @@ public class DatabaseHelper extends SQLiteOpenHelper {
     private final static String DATABASE_NAME = "GOLDEN_SERVICES_RECIBOS";
 
     public DatabaseHelper(Context context) {
-        super(context, DATABASE_NAME, null, 1);
+        super(context, DATABASE_NAME, null, 2);
     }
 
     @Override
     public void onCreate(SQLiteDatabase db) {
-        db.execSQL("create table recibos (desc text, tipo text, id integer primary key)");
+        db.execSQL("create table recibos (desc text, tipo text, id integer primary key, id_usuario integer)");
     }
 
     @Override
@@ -35,25 +37,35 @@ public class DatabaseHelper extends SQLiteOpenHelper {
 
     public void inserirRecibo(String data, Recibo.Tipo tipo) {
         SQLiteDatabase db = this.getWritableDatabase();
-        ContentValues contentValues = new ContentValues();
-        contentValues.put("desc", data);
-        contentValues.put("tipo", tipo.toString());
-        db.insert("recibos", null, contentValues);
+        Usuario u = Helper.getUsuarioConectado();
+        if(u != null) {
+            ContentValues contentValues = new ContentValues();
+            contentValues.put("desc", data);
+            contentValues.put("tipo", tipo.toString());
+            contentValues.put("id_usuario", u.getId());
+            db.insert("recibos", null, contentValues);
+        }
     }
 
     private List<Recibo> obterRecibos(Recibo.Tipo tipo) {
         SQLiteDatabase db = this.getReadableDatabase();
-        Cursor res = db.rawQuery("select * from recibos where tipo = '" + tipo.toString() + "'", null);
-        res.moveToFirst();
+        Usuario u = Helper.getUsuarioConectado();
 
         List<Recibo> ret = new ArrayList<Recibo>();
 
-        while(res.isAfterLast() == false) {
-            Recibo r = new Recibo();
-            r.setDesc(res.getString(res.getColumnIndex("desc")));
-            r.setTipo(Recibo.Tipo.valueOf(res.getString(res.getColumnIndex("tipo"))));
-            ret.add(r);
-            res.moveToNext();
+        if(u != null) {
+
+            Cursor res = db.rawQuery("select * from recibos where tipo = '" + tipo.toString() + "' and id_usuario = " + u.getId(), null);
+            res.moveToFirst();
+
+            while(res.isAfterLast() == false) {
+                Recibo r = new Recibo();
+                r.setDesc(res.getString(res.getColumnIndex("desc")));
+                r.setTipo(Recibo.Tipo.valueOf(res.getString(res.getColumnIndex("tipo"))));
+                ret.add(r);
+                res.moveToNext();
+            }
+
         }
 
         return ret;
